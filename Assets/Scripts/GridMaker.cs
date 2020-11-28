1fb1aa8e1a914b526681c7acffa4a28a3ebe42de
reembyounis <73337305+reembyounis@users.noreply.github.com>
2020-11-28 23:27:17 +0200

    Add files via upload

---
+++ Assets/Scripts/GridMaker.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridMaker : MonoBehaviour
+{
+
+	public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	public GameObject something;
+	Node[,] grid;
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+	public List<Node> path;
+	public List<Node> open=new List<Node>();
+
+	void Start()
+	{
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+		CreateGrid();
+		
+	}
+
+	void CreateGrid()
+    {
+		grid = new Node[gridSizeX, gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+		for (int x = 0; x < gridSizeX; x++)
+		{
+			for (int y = 0; y < gridSizeY; y++)
+			{
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+				grid[x, y] = new Node(walkable, worldPoint,x,y);
+			}
+		}
+
+	}
+
+	public List<Node> Neighbours(Node node)
+	{
+		List<Node> neighbours = new List<Node>();
+		for(int X = node.x - 1; X <= node.x+1; X++)
+        {
+			for(int Y = node.y; Y <= node.y+1; Y++)
+            {
+				if(X == node.x && Y == node.y)
+                {
+					continue;
+                }
+
+				if(X >= 0 && X < gridSizeX && Y >= 0 && Y < gridSizeY)
+                {
+					neighbours.Add(grid[X, Y]);
+                }
+            }
+        }
+		return neighbours;
+	}
+
+	public Node NodeFromWorldPoint(Vector3 worldPosition)
+	{
+		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+		float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+		return grid[x, y];
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+		if (grid != null)
+		{
+			foreach (Node n in grid)
+			{
+				GameObject cube = Instantiate(something, n.worldPosition, Quaternion.identity);
+				cube.transform.localScale = Vector3.one * (nodeDiameter - .1f);
+				Renderer rend = cube.GetComponent<Renderer>();
+				if (!n.walkable)
+                {
+					rend.material.color = Color.red;
+
+				}
+				else if (open.Contains(n) && !path.Contains(n))
+				{
+					rend.material.color = Color.green;
+				}
+
+				else if(path.Contains(n))
+                {
+					rend.material.color = Color.black;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    GridMaker grid;
+    public Transform start;
+    public Transform end;
+
+    void Awake()
+    {
+        grid = GetComponent<GridMaker>();
+    }
+
+    void Update()
+    {
+        startToEnd(start.position, end.position);
+    }
+
+    void startToEnd(Vector3 startPos, Vector3 endPos)
+    {
+        Node start = grid.NodeFromWorldPoint(startPos);
+        Node end = grid.NodeFromWorldPoint(endPos);
+
+        List<Node> opened = new List<Node>();
+        List<Node> closed = new List<Node>();
+        opened.Add(start);
+
+        while (opened.Count != 0)
+        {
+            Node currentPos = opened[0];
+
+            for (int i = 0; i < opened.Count; i++)
+            {
+                int fCostCurrent = currentPos.HCost + currentPos.GCost;
+                int fCostOpened = opened[i].HCost + opened[i].GCost;
+                if (fCostOpened <= fCostCurrent && opened[i].HCost < opened[i].HCost)
+                {
+                    currentPos = opened[i];
+                }
+            }
+            grid.open.Add(currentPos);
+            opened.Remove(currentPos);
+            closed.Add(currentPos);
+
+            if (currentPos == end)
+            {
+                pathIt(start,end);
+                return;
+            }
+
+            foreach(Node neighbour in grid.Neighbours(currentPos))
+            {
+                if(closed.Contains(neighbour) || !neighbour.walkable)
+                {
+                    continue;
+                }
+
+                int newStep = currentPos.GCost + Distance(currentPos, neighbour);
+                if(newStep < neighbour.GCost || !opened.Contains(neighbour))
+                {
+                    neighbour.GCost = newStep;
+                    neighbour.HCost = Distance(neighbour,end);
+
+                    neighbour.parent = currentPos;
+
+                    if (!opened.Contains(neighbour))
+                    {
+                        opened.Add(neighbour);
+                    }
+                }
+            }
+        }
+
+    }
+
+    int Distance(Node start,Node end)
+    {
+        int xDistance = Mathf.Abs(start.x - end.x);
+        int yDistance = Mathf.Abs(start.y - end.y);
+
+        if (xDistance > yDistance)
+        {
+            return 14 * yDistance + 10 * (xDistance - yDistance);
+        }
+
+        else
+        {
+            return 14 * xDistance + 10 * (yDistance - xDistance);
+        }
+    }
+
+    void pathIt(Node start,Node end)
+    {
+        List<Node> path = new List<Node>();
+        Node current = end;
+        while (current != start)
+        {
+            path.Add(current);
+            current = current.parent;
+        }
+        path.Reverse();
+        grid.path = path;
+    }
+   
+}
