4aa97582b0fb28100b58a4f8b17495d9412c58d7
reembyounis <73337305+reembyounis@users.noreply.github.com>
2021-04-08 16:03:30 +0300

    Add files via upload

---
+++ Assets/scripts/Menus/Menus.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menus : MonoBehaviour
+{
+    public void AStarPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void AStarRandom()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void BFSPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void DFSPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void BubbleSort()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+
+    public void InsertionSort()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6);
+    }
+
+    public void MergeSort()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 7);
+    }
+}
---
+++ Assets/scripts/PathFinding/Node.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node {
+	
+	public bool walkable;
+	public Vector3 worldPosition;
+	public int x;
+	public int y;
+	public int gCost;
+	public int hCost;
+	public Node parent;
+	public Renderer renderer;
+	public GameObject game;
+	
+	public Node(bool Walkable, Vector3 WorldPos, int X, int Y) {
+		walkable = Walkable;
+		worldPosition = WorldPos;
+		x = X;
+		y = Y;
+	}
+}
